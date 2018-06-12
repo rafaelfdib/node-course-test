43344e90f35f0b0876c5cf17f49a63420a958055
Rafael Ferrari Dib <rafael@Rafaels-iMac.local>
2018-06-11 22:46:50 -0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+var app = express();
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append to sever log.");
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render("mantinence.hbs");
+// });
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    userName: "Rafael"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({ errorMessage: "Unable to handle request" });
+});
+app.listen(3000, () => {
+  console.log("\x1b[36m%s\x1b[0m", "Server is up on port 3000");
+});
